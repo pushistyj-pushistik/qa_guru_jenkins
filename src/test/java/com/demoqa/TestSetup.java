fd25e4ccdf843c4baa2f2f38418add86a45ab8ee
zhanna sencenko <jannas93@mail.ru>
2022-05-17 00:23:24 +0300

    added config

--- src/test/java/com/demoqa/TestSetup.java
+++ src/test/java/com/demoqa/TestSetup.java
@@ -2,8 +2,10 @@ package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -12,19 +14,19 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 
 public class TestSetup {
+    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
-    static String browser = System.getProperty("browser", "chrome");
+    static String browser = System.getProperty("browser", "safari");
     static String browserSize = System.getProperty("browserSize", "1920x1080");
-    String selenoidServer = System.getProperty("selenoidServer");
 
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        Configuration.browser = browser;
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = browser;
         Configuration.browserSize = browserSize;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://" + config.login() + ":" + config.password() + "@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
